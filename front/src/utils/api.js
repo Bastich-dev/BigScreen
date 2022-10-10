1521a701f9a52db09361139f5b69c91751025ae2
BastosC <bastien.chantrel.pro@gmail.com>
2022-10-10 11:15:26 +0200

    fix

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Config
 // --------------------------------------------------------------------------------------------------------------
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api" : process.env.NEXT_PUBLIC_API_URL;
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api" : process.env.NEXT_PUBLIC_API_URL + "/api";
 
 const withAuth = () => {
   return {
